c67eb1edd5d46e64bdcd3230e24c8c60a95aed71
Jawad Abbasi <Jawadabbasi42@gmail.com>
2023-06-14 09:17:48 +0500

    Add files via upload

---
+++ hjb-resource.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Plugin Name: HJB Resource
+*/
+
+/* Job Custom Post Type */
+require plugin_dir_path(__FILE__) .'/inc/job-post-type.php';
+
+
+/* Job Category Taxonomy */
+require plugin_dir_path(__FILE__) .'/inc/jobcategory-taxomony.php';
